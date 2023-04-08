0381e7c62ebfef593befeddfa346a0aaf7f5ff78
Rodrigo Buges <99702723+rod-buges@users.noreply.github.com>
2023-04-08 16:52:22 -0300

    1049.java

---
+++ 1049.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		Scanner ler = new Scanner(System.in);
+		String palavra1 = ler.next();
+		String palavra2 = ler.next();
+		String palavra3 = ler.next();
+		if (palavra1.equalsIgnoreCase("vertebrado")) {
+			if (palavra2.equalsIgnoreCase("ave")) {
+				if (palavra3.equalsIgnoreCase("carnivoro")) {
+					System.out.println("aguia");
+				} else {
+					System.out.println("pomba");
+				}
+			} else {
+				if (palavra3.equalsIgnoreCase("onivoro")) {
+					System.out.println("homem");
+				} else {
+					System.out.println("vaca");
+				}
+			}
+		} else {
+			if (palavra2.equalsIgnoreCase("inseto")) {
+				if (palavra3.equalsIgnoreCase("hematofago")) {
+					System.out.println("pulga");
+				} else {
+					System.out.println("lagarta");
+				}
+			} else {
+				if (palavra3.equalsIgnoreCase("hematofago")) {
+					System.out.println("sanguessuga");
+				} else {
+					System.out.println("minhoca");
+				}			
+			}
+		}
+	}
+	
+}
