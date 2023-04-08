f411388a9e5cc0e1764bb97b47815583a8e074c6
Rodrigo Buges <99702723+rod-buges@users.noreply.github.com>
2023-04-08 16:38:09 -0300

    Create 1048.java

---
+++ 1048.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+       Scanner leitor = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        double salario = leitor.nextDouble();
+        double reajuste;
+        double novoSalario;
+        int percent;
+
+        if (salario >= 0 && salario <= 400) {
+            reajuste = salario * 0.15;
+            novoSalario = salario + reajuste;
+            percent = 15;
+        } else if (salario > 400 && salario <= 800) {
+            reajuste = salario * 0.12;
+            novoSalario = salario + reajuste;
+            percent = 12;
+        } else if (salario > 800 && salario <= 1200) {
+            reajuste = salario * 0.10;
+            novoSalario = salario + reajuste;
+            percent = 10;
+        } else if (salario > 1200 && salario <= 2000) {
+            reajuste = salario * 0.07;
+            novoSalario = salario + reajuste;
+            percent = 7;
+        } else {
+            reajuste = salario * 0.04;
+            novoSalario = salario + reajuste;
+            percent = 4;
+        }
+
+        System.out.println("Novo salario: " + df.format(novoSalario));
+        System.out.println("Reajuste ganho: " +df.format(reajuste));
+        System.out.println("Em percentual: " + percent + " %");
+ 
+    }
+ 
+}
+//Digite números decimais com vírgula e não com ponto.
+//Desta forma: 800,01
